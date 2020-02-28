1ed11a6471561f63c617fa4d34b117f5d4a0a2bc
Cathy <cathy.healey@gmail.com>
2020-02-27 20:25:16 -0500

    added a docs folder and made updates to package.json scripts

--- src/js/model/Location.js
+++ src/js/model/Location.js
@@ -1,4 +1,3 @@
-console.log("Imported Location module");
 export default class Search {
   constructor(state = 0) {
     this.state = state;
@@ -16,7 +15,6 @@ export default class Search {
       this.lat = latitude;
       this.long = longitude;
       this.state = 1;
-      console.log(`LON - ${this.long} LAT = ${this.lat} state = ${this.state}`);
       // Handle coordinates
     } catch (error) {
       console.error(`get Location error = ${error}`);
